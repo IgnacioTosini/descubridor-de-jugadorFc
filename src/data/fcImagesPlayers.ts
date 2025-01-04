6634c6273da9567b608a4359b14fe934df2b9f1d
ignacio tosini <ignaciotosini2002@gmail.com>
2025-01-04 00:20:24 -0300

    Correccion db

--- src/data/fcImagesPlayers.ts
+++ src/data/fcImagesPlayers.ts
@@ -1,8 +1,8 @@
-import { Image } from "../types";
+import { ImagesPlayer } from "../types";
 
-export async function importAllImages(): Promise<Image[]> {
+export async function importAllImages(): Promise<ImagesPlayer> {
     const modules = import.meta.glob('../assets/fc-Image/*.{png,jpg,jpeg,webp}');
-    const images: Image[] = [];
+    const images: ImagesPlayer = [];
 
     for (const path in modules) {
         const module = await modules[path]() as { default: string };
